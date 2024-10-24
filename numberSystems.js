a210c9c36051e9d964e6cfea623186c528918f54
ninac13 <144480538+ninac13@users.noreply.github.com>
2024-10-24 13:58:40 -0400

    10/24

--- numberSystems.js
+++ numberSystems.js
@@ -1,6 +1,19 @@
-//  Decimal to binary
-// Have the user input a decimal in the function.
-// 
-
-
 // Binary to decimal
+// Have the user input a string value, in binary
+// Use a for loop to iterate through each value in the string until it reaches the last letter
+// Everytime there is a one in the string,
+
+// Decimal to binary
+// Have the user input a number (decimal)
+// Continuously multiply the decimal part of the number by two until the remainder is 0. (use while loop)
+// For each time the whole number part of the number is 1, add a 1 to the answer (string)
+// For each time the whole number part of the number is 0, add a 0 to the answer (string)
+
+function decimalToBinary(decimal){
+    let answer = "";
+    while (remainder != 0){
+        decimal === decimal*2;
+        if (decimal)
+
+    }
+}
