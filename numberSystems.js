71f6314c15f9b110be86a5a45671e6d7eddc4312
ninac13 <144480538+ninac13@users.noreply.github.com>
2024-10-25 13:56:25 -0400

    binary

--- numberSystems.js
+++ numberSystems.js
@@ -3,17 +3,52 @@
 // Use a for loop to iterate through each value in the string until it reaches the last letter
 // Everytime there is a one in the string,
 
-// Decimal to binary
-// Have the user input a number (decimal)
-// Continuously multiply the decimal part of the number by two until the remainder is 0. (use while loop)
-// For each time the whole number part of the number is 1, add a 1 to the answer (string)
-// For each time the whole number part of the number is 0, add a 0 to the answer (string)
-
-function decimalToBinary(decimal){
-    let answer = "";
-    while (remainder != 0){
-        decimal === decimal*2;
-        if (decimal)
-
+function binaryToDec(binary){
+    let decimal = 0; //output initialize
+    for(let i = 0; i<binary.length; i++){
+        if(binary[binary.length - i - 1]==="1"){
+            decimal = decimal + Math.pow(2,i);
+        }
     }
+    return decimal;
 }
+
+console.log(binaryToDec("1100111"));
+
+// Decimal to binary
+
+// function decToBinary(decimal){
+//     let binary = [];
+//     for (let i = 0; i++; decimal >= 1){ //i is the number of times a decimal can be divided by 2
+//         //code will run as long as decimal does not reach 0
+//         if ((decimal%2)===1){ //if the remainder of a decimal being divided by 2 is equal to 1, append that to the space before the previously appended value (this can be i)
+//             binary.push
+//             //make the whole number portion of the decimal divided by 2 as the decimal
+//             decimal = Math.floor(decimal/2);
+//         }
+//         else{ 
+//              // if the remainder of the decimal being divided by 2 is 0, append the zero before the previous appended value (goes first)
+            
+//             //make the whole number portion of the decimal divided by 2 as the decimal
+//             decimal = Math.floor(decimal/2);
+//         }
+//     }
+//     return binary;
+// }
+
+//class function
+
+function decimalToBinary(dec){
+    let binary = "";
+    if(dec === 0){
+        return 0;
+    }
+    while(dec>=1){
+        let remainder = dec%2;
+        binary = remainder +binary;
+        dec = Math.floor(dec/2);
+    }
+    return binary;
+}
+
+console.log(decimalToBinary(9));
